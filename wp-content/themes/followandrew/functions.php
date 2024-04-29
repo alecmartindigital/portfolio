ce6fa22844412b8bbb7d9a2e328b5ae27cde0b95
Alec Martin <alecmartindigital@gmail.com>
2024-04-28 21:15:15 -0400

    finished follow andrew tutorial

--- wp-content/themes/followandrew/functions.php
+++ wp-content/themes/followandrew/functions.php
@@ -1,8 +1,27 @@
 <?php
+function followandrew_theme_support(){
+    //adds dynamic title tag support
+    add_theme_support('title-tag');
+    add_theme_support('custom-logo');
+    add_theme_support('post-thumbnails');
+}
+
+add_action('after_setup_theme', 'followandrew_theme_support');
+
+function followandrew_menus(){
+    $locations = array(
+        'primary' => "Desktop Primary Left Sidebar",
+        'footer' => "Footer menu items"
+    );
+
+    register_nav_menus($locations);
+}
+
+add_action('init', 'followandrew_menus');
 
 function followandrew_register_styles(){
     $version = wp_get_theme()->get('Version');
-    wp_enqueue_style('followandrew-style', get_template_directory_uri() . "/css/style.css", array('followandrew-bootstrap'), $version, 'all');
+    wp_enqueue_style('followandrew-style', get_template_directory_uri() . "/assets/css/style.css", array('followandrew-bootstrap'), $version, 'all');
     wp_enqueue_style('followandrew-bootstrap', "https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css", array(), '4.4.1', 'all');
     wp_enqueue_style('followandrew-fontawesome', "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/css/all.min.css", array(), '5.13.0', 'all');
 }
@@ -17,4 +36,35 @@ function followandrew_register_scripts(){
 }
 
 add_action('wp_enqueue_scripts', 'followandrew_register_scripts');
+
+function followandrew_widget_areas(){
+
+    register_sidebar(
+        array(
+            'before_title' => '',
+            'after_title' => '',
+            'before_widget' => '<ul class="social-list list-inline py-3 mx-auto">',
+            'after_widget' => '</ul>',
+            'name' => 'Sidebar Area',
+            'id' => 'sidebar-1',
+            'description' => 'Sidebar Widget Area'
+        )
+        );
+
+        register_sidebar(
+            array(
+                'before_title' => '',
+                'after_title' => '',
+                'before_widget' => '<ul class="social-list list-inline py-3 mx-auto">',
+                'after_widget' => '</ul>',
+                'name' => 'Footer Area',
+                'id' => 'footer-1',
+                'description' => 'Footer Widget Area'
+            )
+            );
+    
+}
+
+add_action('widgets_init', 'followandrew_widget_areas');
+
 ?>
