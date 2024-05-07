39139834d5e02da965a9a38ceb4187900383f767
Alec Martin <alecmartindigital@gmail.com>
2024-05-06 23:12:38 -0400

    footer and nav css edits

--- wp-content/themes/followandrew/footer.php
+++ wp-content/themes/followandrew/footer.php
@@ -5,25 +5,27 @@
 			<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navigation" aria-controls="navigation" aria-expanded="false" aria-label="Toggle navigation">
 			<span class="navbar-toggler-icon"></span>
 			</button>			
-				<a href="index.html">
+				
 			<div id="navigation" class="" >
+                <a href="index.html">
+                    <div style="height: 50px; width: auto" class="logo">
+                        <?php
+                        if(function_exists('the_custom_logo')){
+                            
+                            $custom_logo_id = get_theme_mod ('custom_logo');
+                            $logo = wp_get_attachment_image_src($custom_logo_id);
+                        }
+                        ?>
+                    </div>
+                </a>
 
-                <?php
-                if(function_exists('the_custom_logo')){
-                    
-                    $custom_logo_id = get_theme_mod ('custom_logo');
-                    $logo = wp_get_attachment_image_src($custom_logo_id);
-                }
-                ?>
-                
-            </a>
                 <?php 
                     wp_nav_menu(
                         array(
                             'menu' => 'primary',
                             'container' => '',
                             'theme_location' => 'primary',
-                            'items_wrap' => '<ul id="" class="">%3$s</ul>'
+                            'items_wrap' => '<ul id="" class="nav-footer">%3$s</ul>'
                         )
                             
                     );
